a7ad9c17c3986c14889bfe69f87f099a2ac451be
huiyeruzhou <2251262734@qq.com>
2025-07-07 15:44:01 +0800

    copyright info

--- taskutils/memory_data/template.py
+++ taskutils/memory_data/template.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Bytedance Ltd. and/or its affiliates
 # Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
